4f7d4b975115550e7e61d5494d8341ce619e97b4
Emre Özbağdatlı <74432853+EmreOzbagdatli@users.noreply.github.com>
2023-09-05 14:27:21 +0300

    HomeWork1

---
+++ HomeWork1.playground/Contents.swift
@@ -0,0 +1,95 @@
+import UIKit
+
+var sehir: String = "Istanbul"
+var ulke: String = "Turkey"
+var telefon: String  = "555 123 45 67"
+var postaKodu: Int  = 34
+var email: String  = "emreozbagdatli@gmail.com"
+var meslek: String  = "Software Engineer"
+var stokMiktari:Int = 100
+var musteriAdi: String  = "Ahmet"
+var bakiye:Float = 789.45
+var dogumGunu: String  = "Pazartesi"
+var maas:Float = 6000
+var medeniDurum:Bool = false
+var urunYorum: String  = "Ürün çok kötü almayın!!!"
+var odemeTarihi: String  = "15 Eylül 2023"
+var odeme: String  = "15 Kasım 2023"
+var siparisAdeti:Int = 10
+var arabaModeli: String  = "Toyota Corolla"
+var kitapAdi: String  = "Beyaz Diş"
+var yayinlamaTarihi: String  = "5 Temmuz 2022"
+var indirimMiktari:Int = 50
+var odaSayisi:Int = 3
+var enlem: String  = "41.0082"
+var boylam: String  = "28.9784"
+var urunAdi: String  = "Spor Ayakkabı"
+var yemekFiyati:Int = 120
+var marka: String  = "Adidas"
+var muzikAdi: String  = "Revenge"
+var videoSuresi:Float = 30.25
+var urunPuani:Float = 4.8
+var resimAdi: String  = "Yıldızlı gece"
+var dosyaFormati: String  = "png"
+var renk: String  = "Yeşil"
+var renkKodu: String  = "#00FF00"
+var telefonModeli: String  = "Samsung Galaxy s1000"
+var ekranBoyutu: Float = 6.1
+var agirlik: Int = 180
+var ulusalGun: String  = "19 Mayıs"
+var tatilGunu: String  = "Ramazan Bayramı"
+var rezervasyonTarihi: String  = "10 Ekim 2023"
+var sokakAdi: String  = "Atatürk Caddesi"
+var otobusHatti: String  = "32A"
+var kalanDakika: Int = 8
+var takipKodu: String  = "XY7ZP2"
+var kuponSuresi: Int = 30
+var kuponKodu: String  = "HULK30"
+var faturaAdresi: String  = "Merkez Mah. Cumhuriyet Cad."
+
+print("Şehir: \(sehir)")
+print("Ülke: \(ulke)")
+print("Telefon: \(telefon)")
+print("Posta Kodu: +\(postaKodu)")
+print("E-posta: \(email)")
+print("Meslek: \(meslek)")
+print("Stok Miktarı: \(stokMiktari)")
+print("Müşteri Adı: \(musteriAdi)")
+print("Bakiye: \(bakiye)")
+print("Doğum Günü: \(dogumGunu)")
+print("Maaş: $\(maas)")
+print("Medeni Durum: \(medeniDurum)")
+print("Ürün Yorumu: \(urunYorum)")
+print("Ödeme Tarihi: \(odemeTarihi)")
+print("Ödeme: \(odeme)")
+print("Sipariş Adeti: \(siparisAdeti)")
+print("Araba Modeli: \(arabaModeli)")
+print("Kitap Adı: \(kitapAdi)")
+print("Yayınlanma Tarihi: \(yayinlamaTarihi)")
+print("İndirim Miktarı: \(indirimMiktari)")
+print("Oda Sayısı: \(odaSayisi)")
+print("Enlem: \(enlem)")
+print("Boylam: \(boylam)")
+print("Ürün Adı: \(urunAdi)")
+print("Yemek Fiyatı: \(yemekFiyati)")
+print("Marka: \(marka)")
+print("Müzik Adı: \(muzikAdi)")
+print("Video Süresi: \(videoSuresi)")
+print("Ürün Puanı: \(urunPuani)")
+print("Resim Adı: \(resimAdi)")
+print("Dosya Formatı: \(dosyaFormati)")
+print("Renk: \(renk)")
+print("Renk Kodu: \(renkKodu)")
+print("Telefon Modeli: \(telefonModeli)")
+print("Ekran Boyutu: \(ekranBoyutu)")
+print("Ağırlık: \(agirlik)")
+print("Ulusal Gün: \(ulusalGun)")
+print("Tatil Günü: \(tatilGunu)")
+print("Rezervasyon Tarihi: \(rezervasyonTarihi)")
+print("Sokak Adı: \(sokakAdi)")
+print("Otobüs Hattı: \(otobusHatti)")
+print("Kalan Dakika: \(kalanDakika)")
+print("Takip Kodu: \(takipKodu)")
+print("Kupon Süresi: \(kuponKodu)")
+print("Kupon Kodu: \(kuponKodu)")
+print("Fatura Adresi: \(faturaAdresi)")
